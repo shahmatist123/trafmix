564d0b640c63f4ac80c191aa98627b6fc899b2c9
khggc <khggcvfd@gmail.com>
2022-01-31 13:05:41 +0500

    first commit

--- Controller/mail-controller.js
+++ Controller/mail-controller.js
@@ -1,20 +1,31 @@
 const nodemailer = require('nodemailer')
 const config = require('config')
 exports.forgetPassword  = (req, res) => {
-    let transporter = nodemailer.createTransport({
-        sendmail: true,
-        newline: 'unix',
-        path: '/usr/sbin/sendmail'
+    async function send (){
+        let testAccount = await nodemailer.createTestAccount();
+        let transporter = nodemailer.createTransport({
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: testAccount.user, // generated ethereal user
+                pass: testAccount.pass, // generated ethereal password
+            },
+        });
+       const info = await transporter.sendMail({
+            from: 'sender@example.com',
+            to: req.body.email,
+            subject: 'Message',
+            text: 'I hope this message gets delivered!'
+        }, (err, info) => {
+            console.log(info);
+            console.log(info);
+        });
+       console.log(info.messageId)
+    }
+    send().catch(error =>{
+        console.log(error)
     });
-    transporter.sendMail({
-        from: 'sender@example.com',
-        to: req.body.email,
-        subject: 'Message',
-        text: 'I hope this message gets delivered!'
-    }, (err, info) => {
-        console.log(info);
-        console.log(info);
-    });
+
 
 
 
